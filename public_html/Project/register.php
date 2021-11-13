3b5fb254c7a5b6352a152b62c8bdf708a6831264
MPR-GH <mihirrana002@gmail.com>
2021-11-12 20:05:02 -0500

    login fixed

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -2,33 +2,6 @@
 require(__DIR__ . "/../../partials/nav.php");
 reset_session();
 ?>
-<form onsubmit="return validate(this)" method="POST">
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" required />
-    </div>
-    <div>
-        <label for="username">Username</label>
-        <input type="text" name="username" required maxlength="30" />
-    </div>
-    <div>
-        <label for="pw">Password</label>
-        <input type="password" id="pw" name="password" required minlength="8" />
-    </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
-    <input type="submit" value="Register" />
-</form>
-<script>
-    function validate(form) {
-        //TODO 1: implement JavaScript validation
-        //ensure it returns false for an error and true for success
-
-        return true;
-    }
-</script>
 <?php
 //TODO 2: add PHP Code
 if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
@@ -90,6 +63,45 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
     }
 }
 ?>
+<?php
+$email = get_user_email();
+$username = get_username();
+?>
+<form method="POST" onsubmit="return validate(this)">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" value="" required />
+    </div>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" name="username" value="" required maxlength="30" />
+    </div>
+    <div>
+        <label for="pw">Password</label>
+        <input type="password" id="pw" name="password" required minlength="8" />
+    </div>
+    <div>
+        <label for="confirm">Confirm</label>
+        <input type="password" name="confirm" required minlength="8" />
+    </div>
+    <input type="submit" value="Register" />
+</form>
+<script>
+    function validate(form) {
+        //TODO 1: implement JavaScript validation
+        //ensure it returns false for an error and true for success
+        let pw = form.password.value;
+        let con = form.confirm.value;
+        let isValid = true;
+        form.email.value = "<?php se($email); ?>";
+        form.username.value = "<?php se($username); ?>";
+        if (pw !== con) {
+            flash("Password and Confirm password must match", "warning");
+            isValid = false; 
+        }
+        return isValid;
+    }
+</script>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
