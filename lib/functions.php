5fec7630afe58c30470870bf7105ccf0ce4bdc06
MPR-GH <mihirrana002@gmail.com>
2021-12-19 23:23:37 -0500

    redirect fix

--- lib/functions.php
+++ lib/functions.php
@@ -41,7 +41,7 @@ function is_logged_in($redirect = false, $destination = "login.php")
     $isLoggedIn = isset($_SESSION["user"]);
     if ($redirect && !$isLoggedIn) {
         flash("You must be logged in to view this page", "warning");
-        die(header("Location: $destination"));
+        redirect($destination);
     }
     return $isLoggedIn; //se($_SESSION, "user", false, false);
 }
